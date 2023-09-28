cce53302f92f3ccfe27e87b57edf3c0a00f214d3
Muhammad Amin Haiqal <73211654+aminhaiqal@users.noreply.github.com>
2023-09-29 00:34:18 +0800

    add .gitignore

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 const app = express();
+const firebaseConfig = require('./configs/firebase');
+
 const port = 3000;
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 app.get('/', (req, res) => {
   res.send('Hello, Express.js!');
